a720ca0b391cc8a2b047e6faf73a3437c6e6d883
vfontirroig <vfontirroig@pm.me>
2026-05-29 11:51:59 +0100

    changed path

--- experiments/euclid-cosmos/visualization/run_umap.sh
+++ experiments/euclid-cosmos/visualization/run_umap.sh
@@ -16,5 +16,5 @@ CKPT_DIR="/n03data/fontirro/checkpoints/euclid-cosmos-phase1"
 python /n03data/fontirro/galaxy-counter/experiments/euclid-cosmos/visualization/umap_latent.py \
     --checkpoint "${CKPT_DIR}/best-epoch=21-step=98000.ckpt" \
     --h5         "/n03data/fontirro/data_files/euclid_cosmos_pairs.h5" \
-    --out        "/n03data/fontirro/plots_model/euclid-cosmos-phase1/umap_val_full_no_cutouts-1.png" \
+    --out        "/n03data/fontirro/plots_model/euclid-cosmos-phase1/test/umap_val_full_no_cutouts-1.png" \
     --n-samples -1
