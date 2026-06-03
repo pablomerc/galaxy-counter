40c810e3a3335ddd512502ec0466b58ddbe7a4e8
vfontirroig <vfontirroig@pm.me>
2026-06-03 11:52:42 +0100

    added parallelization

--- experiments/euclid-cosmos/visualization/run_easy_fits.sh
+++ experiments/euclid-cosmos/visualization/run_easy_fits.sh
@@ -3,14 +3,14 @@
 #SBATCH --output=/home/fontirro/logs/euclid_cosmos_easy_fits_%j.out
 #SBATCH --error=/home/fontirro/logs/euclid_cosmos_easy_fits_%j.err
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4      
-#SBATCH --mem=8GB
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32GB
 #SBATCH --time=1-00:00:00
-#SBATCH --partition=pscompl      # change to your GPU partition
+#SBATCH --partition=pscompl
 set -euo pipefail
 
+SCRIPT=/n03data/fontirro/galaxy-counter/experiments/euclid-cosmos/visualization/easy_fits.py
+#OUT_DIR=/n03data/fontirro/output_plots
 
-
-for idx in $(seq 0 311105); do
-    python /n03data/fontirro/galaxy-counter/experiments/euclid-cosmos/visualization/easy_fits.py --idx $idx
-done
+seq 0 311105 | parallel -j "$SLURM_CPUS_PER_TASK" \
+    python "$SCRIPT" --idx {}
