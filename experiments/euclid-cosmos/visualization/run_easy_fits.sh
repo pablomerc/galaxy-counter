896fbe0296241c6446fbeaea527800d1413f59e7
vfontirroig <vfontirroig@pm.me>
2026-06-03 11:10:45 +0100

    created script to iterate over indices

---
+++ experiments/euclid-cosmos/visualization/run_easy_fits.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=euclid_cosmos_easy_fits
+#SBATCH --output=/home/fontirro/logs/euclid_cosmos_easy_fits_%j.out
+#SBATCH --error=/home/fontirro/logs/euclid_cosmos_easy_fits_%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4      
+#SBATCH --mem=8GB
+#SBATCH --time=1-00:00:00
+#SBATCH --partition=pscompl      # change to your GPU partition
+set -euo pipefail
+
+
+
+for idx in range(311106):
+    python /n03data/fontirro/galaxy-counter/experiments/euclid-cosmos/visualization/easy_fits.py --idx $idx
