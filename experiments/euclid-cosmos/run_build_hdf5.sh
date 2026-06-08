6987652bc68748574824501c4563c007861b0608
vfontirroig <vfontirroig@pm.me>
2026-06-08 09:30:06 +0100

    updated job running time

--- experiments/euclid-cosmos/run_build_hdf5.sh
+++ experiments/euclid-cosmos/run_build_hdf5.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32        # set NUM_WORKERS in build_hdf5.py to match this
 #SBATCH --mem=64G
-#SBATCH --time=04:00:00
+#SBATCH --time=08:00:00
 #SBATCH --partition=compl
 set -euo pipefail
 
